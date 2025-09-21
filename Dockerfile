356609af633efc18d4bc2b6f4a01755f2c71caf5
vianAja <najwanoctavian@gmail.com>
2025-09-21 15:21:35 +0700

    update dockerfile to run python manage.py

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+RUN python manage.py collectstatic --noinput
+
 CMD ["gunicorn", "-c", "gunicorn-cfg.py", "config.wsgi:application"]
--- config/settings.py
+++ config/settings.py
@@ -72,15 +72,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # ⬅️ tambahkan setelah SecurityMiddleware
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "config.urls"
 
